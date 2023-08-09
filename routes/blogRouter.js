1cb0c6f9222497a5ab4bc589b01bb0705f5e16d6
Bara sofiane badis <barasofianebadis@gmail.com>
2023-08-09 19:51:50 +0100

    Crud Blogs

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const createBlog = require('../controller/BlogController');
+const {createBlog,findBlog,getAllBlog,updateBlog, deleteBlog} = require('../controller/BlogController');
 const {authMiddleware,isAdmin} = require('../middlewares/authMiddlewares');
 
 
 const router=express.Router();
 
 router.post("/create-new-blog",authMiddleware,isAdmin,createBlog);
+router.get("/",getAllBlog);
+router.get("/find-blog",findBlog);
+router.put("/update-blog",authMiddleware,isAdmin,updateBlog);
+router.delete("/delete-blog",authMiddleware,isAdmin,deleteBlog);
 
 module.exports=router;
