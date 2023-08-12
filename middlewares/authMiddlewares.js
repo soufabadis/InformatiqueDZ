b3815e2bf62c8d034e8aef221ada62253f56d798
Bara sofiane badis <barasofianebadis@gmail.com>
2023-08-12 22:09:39 +0100

    logger

--- middlewares/authMiddlewares.js
+++ middlewares/authMiddlewares.js
@@ -11,35 +11,46 @@ const privateKey = fs.readFileSync(privateKeyPath, "utf-8");
 const authMiddleware = asyncHandler(async (req, res, next) => {
   let token;
   if (req?.headers?.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
-    if (token) {
-      const decoded = jwt.verify(token, privateKey);
-      const userEmail = decoded.email; // Extract the email from the decoded JWT payload
-      const user = await Users.findOne({ email: userEmail }); // Find the user by email in the database
-      
-      req.user = user;
-      next(); // Proceed to the next middleware or route handler
-    } else {
-      // No token found in the header
-      throw new Error("Token expired, please log in again.");
-    }
+      token = req.headers.authorization.split(" ")[1];
+      if (token) {
+          try {
+              const decoded = jwt.verify(token, privateKey);
+              const userEmail = decoded.email;
+              const user = await Users.findOne({ email: userEmail });
+              
+              if (user) {
+                  req.user = user;
+                  next();
+              } else {
+                  throw new Error("Token expired, please log in again.");
+              }
+          } catch (error) {
+              throw new Error("Token expired, please log in again.");
+          }
+      } else {
+          throw new Error("Token expired, please log in again.");
+      }
   } else {
-    // No token found in the header
-    throw new Error("Token not found in headers.");
+      logger.error("Token not found in headers.");
+      throw new Error("Token not found in headers.");
   }
 });
- 
-// check Admin user 
 
 const isAdmin = asyncHandler(async (req, res, next) => {
-  email = req.user.email;
-  const adminUser = await Users?.findOne({ email: email});
-  if (adminUser.role !== "admin") {
-    throw new Error("you are not an admin");
-  } else {
-    next();
+  const email = req.user.email;
+  try {
+      const adminUser = await Users?.findOne({ email: email });
+      if (adminUser && adminUser.role === "admin") {
+          logger.info(`User ${email} is an admin.`);
+          next();
+      } else {
+          logger.error(`User ${email} is not an admin.`);
+          throw new Error("You are not an admin.");
+      }
+  } catch (error) {
+      logger.error("Error checking admin role:", error);
+      throw new Error("Error checking admin role.");
   }
 });
 
-
-module.exports = { authMiddleware,isAdmin};
+module.exports = { authMiddleware, isAdmin };
